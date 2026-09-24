5a5cd39e4f9c64aa22d6b8c8a6c0fce7a24e5539
dathito108-hue <dathito108@gmail.com>
2026-09-24 10:16:08 +0700

    M19A: add host BuildConfig stub

---
+++ android/app/host-test/M19ABuildConfigStub.kt
@@ -0,0 +1,9 @@
+package ai.vn97.app
+
+internal object BuildConfig {
+    const val APPLICATION_ID =
+        "ai.vn97.app"
+    const val VERSION_CODE = 190100
+    const val VERSION_NAME =
+        "1.0.0-rc1"
+}
